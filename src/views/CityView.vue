48d42d31ed2bacca411a8e7d58e48a3df5b2f6a6
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-28 16:01:48 -0500

    create cityView view

---
+++ src/views/CityView.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <Suspense>
+            <AsyncCityView />
+            <template #fallback>
+                <p>Loading...</p>
+            </template>
+        </Suspense>
+    </main>
+</template>
+
+<script setup lang="ts">
+import AsyncCityView from '../components/AsyncCityView.vue';
+</script>
