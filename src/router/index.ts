07d943b31c4fbb0fb5105cb2cd873f53bc0b0c63
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-28 16:05:16 -0500

    create city view route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory, Router, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import CityViewVue from "../views/CityView.vue";
 
 const routes: RouteRecordRaw[] = [{
   path: "/",
   name: "home",
   component: HomeView,
-}];
+},
+{
+  path: "/weather/:country/:city",
+  name: "cityView",
+  component: CityViewVue,
+}
+];
 
 const router: Router = createRouter({
   history: createWebHistory(),
